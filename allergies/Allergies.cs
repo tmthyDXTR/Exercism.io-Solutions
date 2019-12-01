b75315d3ac734b8de51789f1678eecf93b1c4a85
tmthyDXTR <langmi89@gmail.com>
2019-12-01 19:57:59 +0100

    1/12/

--- allergies/Allergies.cs
+++ allergies/Allergies.cs
@@ -1,30 +1,37 @@
 using System;
+using System.Linq;
 
 public enum Allergen
 {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128
 }
 
 public class Allergies
 {
+    private static int Value { get; set; }
+
     public Allergies(int mask)
     {
+        Value = mask;
     }
 
     public bool IsAllergicTo(Allergen allergen)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return ((Allergen)Value & allergen) == allergen;
     }
 
     public Allergen[] List()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Enum.GetValues(typeof(Allergen))
+            .Cast<Allergen>()
+            .Where(x => ((int)x & Value) > 0)
+            .ToArray();
     }
 }
